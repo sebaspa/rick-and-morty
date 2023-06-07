a86c8c3cd7526282369c507ed5d0a156cf5189e7
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-07 08:27:11 -0500

    Se cambia las reglas de eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react/recommended',
+    'standard-with-typescript'
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
   rules: {
-    'react-refresh/only-export-components': 'warn'
   }
 }
