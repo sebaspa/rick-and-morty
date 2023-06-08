05a1fa363c6822416fd7271f75ea8156fc734808
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-08 12:05:26 -0500

    Personaje Listado de informacion de el personaje

---
+++ src/store/apis/locationsApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { type ILocationDetail } from '../../types'
+
+export const locationsApi = createApi({
+  reducerPath: 'locations',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_RICK_MORTY_API
+  }),
+  /**
+   * Creates an object with various endpoint functions to query the API.
+   *
+   * @param {Function} builder - A function to build the endpoint object.
+   * @returns {Object} An object with various endpoint functions.
+   */
+  endpoints: (builder) => ({
+    getLocationById: builder.query<ILocationDetail, string>({
+      query: (id) => `/location/${id}`,
+      keepUnusedDataFor: 300
+    })
+  })
+})
+
+export const { useGetLocationByIdQuery } = locationsApi
