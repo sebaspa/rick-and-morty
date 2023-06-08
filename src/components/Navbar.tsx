427c20ef574ad2219308d94dc8411f50a475909d
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-08 13:56:20 -0500

    Crear estado de el menu mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/Rick_and_Morty_logo.webp'
+import { useState } from 'react'
 
 export const Navbar = (): JSX.Element => {
+  const [showMobileMenu, setshowMobileMenu] = useState(false)
+  const handleMobileMenu = (): void => {
+    setshowMobileMenu(!showMobileMenu)
+  }
   return (
     <>
       <nav className="bg-white border-gray-200">
@@ -10,6 +15,7 @@ export const Navbar = (): JSX.Element => {
             <img src={logo} className="h-12 mr-3" alt="Flowbite Logo" />
           </Link>
           <button
+            onClick={handleMobileMenu}
             data-collapse-toggle="navbar-default"
             type="button"
             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
@@ -31,27 +37,22 @@ export const Navbar = (): JSX.Element => {
               ></path>
             </svg>
           </button>
-          <div className="w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-              <li>
-                <NavLink
-                  to="/"
-                  className="mainMenu__a"
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/favorites"
-                  className="mainMenu__a"
-                >
-                  Favoritos
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+          {showMobileMenu && (
+            <div className="w-full md:block md:w-auto" id="navbar-default">
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+                <li>
+                  <NavLink to="/" className="mainMenu__a" aria-current="page">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/favorites" className="mainMenu__a">
+                    Favoritos
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </>
