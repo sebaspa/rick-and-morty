d199203f50436dffa656138ff677fb4f1f67d80c
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-13 16:53:24 -0500

    Pruebas PRueba para el personaje Prueba para la pagina de personajes Prueba para el navbar

--- src/components/Paginate.tsx
+++ src/components/Paginate.tsx
@@ -37,20 +37,25 @@ export const Paginate: React.FC<PaginateProps> = ({
     <>
       <div className="flex flex-col items-center my-5">
         <span className="text-sm text-gray-700 dark:text-gray-400">
-          Showing{' '}
-          <span className="font-semibold text-gray-900 dark:text-white">{page}</span>{' '}
-          to{' '}
+          Showing
+          <span className="font-semibold text-gray-900 dark:text-white">
+            {page}
+          </span>
+          to
           <span className="font-semibold text-gray-900 dark:text-white">
             {itemsCount}
-          </span>{' '}
-          of{' '}
+          </span>
+          of
           <span className="font-semibold text-gray-900 dark:text-white">
             {itemsTotal}
-          </span>{' '}
+          </span>
           Entries
         </span>
         <div className="inline-flex mt-2 xs:mt-0">
-          <button onClick={handlePrevPage} className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <button
+            onClick={handlePrevPage}
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
             <svg
               aria-hidden="true"
               className="w-5 h-5 mr-2"
@@ -66,7 +71,10 @@ export const Paginate: React.FC<PaginateProps> = ({
             </svg>
             Prev
           </button>
-          <button onClick={handleNextPage} className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <button
+            onClick={handleNextPage}
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
             Next
             <svg
               aria-hidden="true"
